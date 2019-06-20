a12f423d3fe28a33b209bd733d8290e62c4ba0cf
Stefan Freudenberg <stefan@agaric.com>
2019-06-20 14:09:15 +0200

    Add jenkins script

---
+++ jenkins.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -xe
+
+vagrant up --provider lxc --provision
+
+trap "vagrant destroy --force" EXIT
+
+vagrant ssh -c "cd /vagrant && sudo python3 setup.py sdist" -- -q
+
+vagrant ssh-config > ${WORKSPACE}/vagrant.ssh-config
+scp -F ${WORKSPACE}/vagrant.ssh-config default:/vagrant/dist/\*.tar.gz ${WORKSPACE}/
+
+devpi use https://devpi.zeit.de/premium-dwh/default
+devpi login premium-dwh --password 'premium-dwh'
+devpi upload *.tar.gz
